119a7f878c77acd22bb65eca85726f93fe95313a
Sheetal Kumar <sheetalkumar105@gmail.com>
2019-01-28 00:17:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/impulsiveweb/galleryimageview/MainActivity.java
@@ -0,0 +1,113 @@
+package com.impulsiveweb.galleryimageview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.impulsiveweb.galleryview.GalleryViewActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int PICK_IMAGE = 100;
+    public static int PICK_VIDEO = 101;
+    public ArrayList<String> paths = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnAddVideo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, PICK_VIDEO);
+            }
+        });
+
+        findViewById(R.id.btnAddImage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, PICK_IMAGE);
+            }
+        });
+
+        findViewById(R.id.btnShowGallery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showGallery();
+            }
+        });
+    }
+
+
+    public void updateList(){
+        ViewGroup vg = (ViewGroup) findViewById(R.id.container);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_VIDEO) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                String selectedVideoPath = getVideoPath(contentURI);
+                Log.d("path",selectedVideoPath);
+                paths.add(selectedVideoPath);
+            }
+        }
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                String selectedImagePath = getImagePath(contentURI);
+                Log.d("path",selectedImagePath);
+                paths.add(selectedImagePath);
+            }
+        }
+    }
+
+    public String getVideoPath(Uri uri) {
+        String[] projection = { MediaStore.Video.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
+    }
+
+    public String getImagePath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
+    }
+
+    public void showGallery(){
+        Intent intent = new Intent(this, GalleryViewActivity.class);
+        intent.putExtra("items", paths);
+        startActivity(intent);
+    }
+
+}
---
+++ galleryview/src/main/java/com/impulsiveweb/galleryview/GalleryViewActivity.java
@@ -0,0 +1,214 @@
+package com.impulsiveweb.galleryview;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
+import java.util.ArrayList;
+
+import cn.jzvd.JZVideoPlayerStandard;
+
+public class GalleryViewActivity extends AppCompatActivity {
+
+    public static final int PERMISSION_REQUEST = 111;
+
+    ArrayList<String> filelist = new ArrayList<>();
+    int position=0;
+    private GalleryPagerAdapter _adapter;
+    ViewPager _pager;
+    ImageView _closeButton;
+    public String TAG = GalleryViewActivity.this.getClass().getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_view);
+        Bundle b=getIntent().getExtras();
+        _pager= (ViewPager) findViewById(R.id.pager);
+        _closeButton = (ImageView) findViewById(R.id.btn_close);
+        position=b.getInt("selection",0);
+        filelist=  b.getStringArrayList("items");
+
+
+
+        _closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Close", "Close clicked");
+                finish();
+            }
+        });
+        if(checkWriteExternalPermission())
+            _init();
+        else
+            grantPermission();
+
+
+    }
+
+
+    private boolean checkWriteExternalPermission()
+    {
+        String permission = android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        int res = checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void grantPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSION_REQUEST);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+
+        switch (requestCode) {
+            case PERMISSION_REQUEST: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    _init();
+                }
+            }
+        }
+    }
+
+
+    public void _init(){
+
+
+        _adapter = new GalleryPagerAdapter(this);
+        _pager.setAdapter(_adapter);
+        _pager.setOffscreenPageLimit(3); // how many images to load into memory_pager
+        _pager.setCurrentItem(position);
+
+        _pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+
+    class GalleryPagerAdapter extends PagerAdapter {
+
+        Context _context;
+        LayoutInflater _inflater;
+
+        boolean isFullscreen = false;
+
+        public GalleryPagerAdapter(Context context) {
+            _context = context;
+            _inflater = (LayoutInflater) _context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return filelist.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == ((LinearLayout) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, final int position) {
+            String path = filelist.get(position);
+            String extension = path.substring(path.lastIndexOf("."));
+
+            Log.e("Extension",extension);
+
+            View itemView = null;
+            if(extension.equalsIgnoreCase(".mp4") || extension.equalsIgnoreCase(".avi") || extension.equalsIgnoreCase(".mkv")){
+                itemView = _inflater.inflate(R.layout.pager_video_item, container, false);
+                container.addView(itemView);
+
+
+
+                final JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) itemView.findViewById(R.id.videoplayer);
+                jzVideoPlayerStandard.setUp(path,
+                        JZVideoPlayerStandard.SCREEN_WINDOW_LIST,
+                        "");
+                Glide.with(_context).load(path)
+                        .into(jzVideoPlayerStandard.thumbImageView);
+
+
+
+            }else{
+                itemView = _inflater.inflate(R.layout.pager_gallery_item, container, false);
+                container.addView(itemView);
+                final SubsamplingScaleImageView imageView =
+                        (SubsamplingScaleImageView) itemView.findViewById(R.id.image);
+                Glide.with(getApplicationContext())
+                        .asBitmap()
+                        .load(filelist.get(position))
+                        .into(new SimpleTarget<Bitmap>() {
+                            @Override
+                            public void onResourceReady(Bitmap image,
+                                                        Transition<? super Bitmap> transition) {
+
+                                int xDim=image.getWidth();
+                                int yDim=image.getHeight();
+                                if(xDim<=4096 && yDim <=4096){
+                                    imageView.setImage(ImageSource.bitmap(image));
+                                }else{
+                                    if(xDim>yDim){
+                                        int nh = (int) ( image.getHeight() * (4096f / image.getWidth()) );
+                                        Bitmap scaled = Bitmap.createScaledBitmap(image, 4096, nh, true);
+                                        imageView.setImage(ImageSource.bitmap(scaled));
+                                    }else{
+                                        int nh = (int) ( image.getWidth() * (4096f / image.getHeight()) );
+                                        Bitmap scaled = Bitmap.createScaledBitmap(image, nh,4096 , true);
+                                        imageView.setImage(ImageSource.bitmap(scaled));
+                                    }
+                                }
+
+                            }
+                        });
+            }
+
+
+            return itemView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((LinearLayout) object);
+        }
+    }
+}
